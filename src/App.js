ebe6c4bf3c85bf69568cd447e80c0b368524951e
MiLee777 <midyashka1506@gmail.com>
2024-10-02 21:46:22 +0700

    feat: add welcome section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Header } from './Components/Header';
+import { Welcome } from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Welcome />
     </div>
   );
 }
---
+++ src/Components/Welcome.js
@@ -0,0 +1,11 @@
+export const Welcome = () => {
+  return (
+    <section className="welcome">
+      <div className="welcome__container">
+        <div className="welcome__content">
+          <h1 className="welcome__title">Welcome <br/> to the Brooklyn <br/> Library</h1>
+        </div>
+      </div>
+    </section>
+  )
+}
