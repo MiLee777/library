085dcf4c774b2171d4151bde0e462558ddfef124
MiLee777 <midyashka1506@gmail.com>
2025-02-13 14:27:03 +0700

    feat: change style basket icon

--- src/Components/Cart/BasketIcon.js
+++ src/Components/Cart/BasketIcon.js
@@ -7,7 +7,7 @@ export const BasketIcon = () => {
   return (
     <>
       <BsBasket3Fill size="28px" color="#fff" />
-      <span>{totalQuantity === 0 ? "" : totalQuantity}</span>
+      <span className="basket__icon-count">{totalQuantity === 0 ? "" : totalQuantity}</span>
     </>
   )
 }
